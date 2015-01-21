b19e0f1d6536100cb01d307b140dcc2b6bddbb13
ahmad-abdelsatar <ahmed.abdsatar@gmail.com>
2015-01-22 01:08:30 +0200

    First try to Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        matInv <- NULL
+        
+        set <- function(y){
+                
+                x <<- y
+                matInv <<- NULL
+        }
+        
+        get <- function() x
+        
+        
 
 }
 
